d6c9ca0aef1559b63a7fc5b554554282f30dd509
Dalton Stichtenoth <jdaltonsticht25@gmail.com>
2019-09-12 11:18:49 -0400

    Added README and hw01 folder

---
+++ hw01/EtchASketch.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import argparse
+import curses
+from curses import wrapper
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("xDim", help = "The Width of the Etch A Sketch board", type = int)
+parser.add_argument("yDim", help = "The Height of the Etch A Sketch board", type = int)
+args = parser.parse_args()
+
+def main(stdscr):
+    xPos = 1
+    yPos = 1
+    for i in range (1, args.xDim+1):
+        stdscr.addstr(0,2*i+1,'{} '.format(i))
+    for i in range (1, args.yDim+1):
+        stdscr.addstr(i,0,'{}:'.format(i))
+    stdscr.addstr(1,3,'X')
+    while True:
+        stdscr.refresh()
+        input = stdscr.getch()
+        if input == ord('w'):
+            if yPos == 1:
+                stdscr.refresh()
+            else:
+                yPos = yPos - 1
+                stdscr.addstr(yPos,2*xPos+1,'X')
+        elif input == ord('d'):
+            if xPos == args.xDim:
+                stdscr.refresh()
+            else:
+                xPos = xPos + 1
+                stdscr.addstr(yPos,2*xPos+1,'X')
+        elif input == ord('s'):
+            if yPos == args.yDim:
+                stdscr.refresh()
+            else:
+                yPos = yPos + 1
+                stdscr.addstr(yPos,2*xPos+1,'X')
+        elif input == ord('a'):
+            if xPos == 1:
+                stdscr.refresh()
+            else:
+                xPos = xPos - 1
+                stdscr.addstr(yPos,2*xPos+1,'X')
+        elif input == ord('q'):
+            break
+        elif input == ord('e'):
+            stdscr.clear()
+            for i in range (1, args.xDim+1):
+                stdscr.addstr(0,2*i+1,'{} '.format(i))
+            for i in range (1, args.yDim+1):
+                stdscr.addstr(i,0,'{}:'.format(i))
+
+wrapper(main)
---
+++ hw01/README.txt
@@ -0,0 +1,9 @@
+All of the 6 Items listed on the HW assignment have been completed
+The latest image of 9/1/19 has been installed on the Beagle Bone
+I have a Virtual Machine Running Ubuntu that I can work on
+The SD cards, cables, and the box from the part room have all been received
+Git is installed and running, but I honestly have no idea how to use it
+The Etch A Sketch program is running:
+Move with the wasd keys
+Quit the program with 'q'
+Reset all the X's with the 'e' key

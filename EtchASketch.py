0db2971fa7bd105b1a71fa44cfd53e2a8a0293b2
Dalton Stichtenoth <jdaltonsticht25@gmail.com>
2019-09-12 01:02:54 -0400

    file complete

--- EtchASketch.py
+++ EtchASketch.py
@@ -13,9 +13,10 @@ def main(stdscr):
     xPos = 1
     yPos = 1
     for i in range (1, args.xDim+1):
-        stdscr.addstr(0,2*i+1,'{} '.format(i))
+        stdscr.addstr(0,2*i,'{} '.format(i))
     for i in range (1, args.yDim+1):
         stdscr.addstr(i,0,'{}:'.format(i))
+    stdscr.addstr(1,2,'X')
     while True:
         stdscr.refresh()
         input = stdscr.getch()

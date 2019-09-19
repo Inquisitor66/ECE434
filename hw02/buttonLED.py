7c7a82839ce86dfd554cc2d10fccb779337a9e05
Demo User <debian@beaglebone.localdomain>
2019-09-19 15:27:38 +0000

    Fixed GPIO mappings

--- hw02/buttonLED.py
+++ hw02/buttonLED.py
@@ -5,14 +5,14 @@ GPIO.setup("P8_17", GPIO.OUT)
 GPIO.setup("P8_11", GPIO.OUT)
 GPIO.setup("P8_9", GPIO.OUT)
 GPIO.setup("P8_7", GPIO.OUT)
-GPIO.setup("P8_8", GPIO.IN)
+GPIO.setup("P8_14", GPIO.IN)
 GPIO.setup("P8_10", GPIO.IN)
 GPIO.setup("P8_12", GPIO.IN)
 GPIO.setup("P9_11", GPIO.IN)
 
 def buttonEvent(button):
-    if button == "P8_8":
-        GPIO.output("P8_7",GPIO.input("P8_8"))
+    if button == "P8_14":
+        GPIO.output("P8_7",GPIO.input("P8_14"))
 	print("Light\n")
     if button == "P8_10":
         GPIO.output("P8_9",GPIO.input("P8_10"))
@@ -22,7 +22,7 @@ def buttonEvent(button):
 	print("Light\n")
         GPIO.output("P8_17",GPIO.input("P9_11"))
 
-GPIO.add_event_detect("P8_8", GPIO.BOTH, callback = buttonEvent)
+GPIO.add_event_detect("P8_14", GPIO.BOTH, callback = buttonEvent)
 GPIO.add_event_detect("P8_10", GPIO.BOTH, callback = buttonEvent)
 GPIO.add_event_detect("P8_12", GPIO.BOTH, callback = buttonEvent)
 GPIO.add_event_detect("P9_11", GPIO.BOTH, callback = buttonEvent)
